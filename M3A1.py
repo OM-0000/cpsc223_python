73af3ea8f1784b3eb3e80cc6d6a8f829950ef9ad
Ossama Mostafa <omostafa1513@gmail.com>
2026-09-03 15:03:50 +0000

    First day of python lecture

---
+++ M3A1.py
@@ -0,0 +1,15 @@
+#! /home/codespace/.python/current/bin/python
+
+# Name: Ossama Mostafa
+# Student ID: 871567681
+# Section: 
+# Assignment: 
+if __name__ == "__main__":
+    print("this is main")
+else:
+    print("not main")
+print("Hello, World!")
+msg=5
+print(msg)
+msg = 5 ** 2
+print(msg)
---
+++ inclassvariables.py
@@ -0,0 +1,6 @@
+x,y,z = 1,2,3
+print(f"x is {x} and y is {y} and z is {z}")
+
+print("What is your name? ")
+name = input()
+print(f"Your name is {name}")
