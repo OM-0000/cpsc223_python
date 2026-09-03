0771f9078b5562e65f24c0f5d7f177b9ef5cb494
Ossama Mostafa <omostafa1513@gmail.com>
2026-09-03 15:22:06 +0000

    m1a2 done

---
+++ M1A2.py
@@ -0,0 +1,8 @@
+# Name: Ossama Mostafa
+# Student ID: 871567681
+# Section: 18254
+# Assignment: Module 1 Assignment 1
+
+print("What is your name? ")
+name_str = input()
+print(f"It's dangerous to go alone {name_str}, take this!")
