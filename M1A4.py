71dc544f6c35ca9088548ad41ac6fdbffe7a47c0
Ossama Mostafa <omostafa1513@gmail.com>
2026-09-03 15:47:01 +0000

    m1a4 done

---
+++ M1A4.py
@@ -0,0 +1,13 @@
+# Name: Ossama Mostafa
+# Student ID: 871567681
+# Section: 18254
+# Assignment: Module 1 Assignment 4
+
+
+
+catchphrase_str = input("Please enter a catchphrase: ")
+
+print(f"[{catchphrase_str.upper()}]")
+print(f"[{catchphrase_str.lower()}]")
+print(f"[{catchphrase_str.lstrip()}]")
+print(f"[{catchphrase_str.rstrip()}]")
