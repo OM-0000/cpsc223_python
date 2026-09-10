de783937acacb21ad608aa16e4889bcc4ec2fbb2
Ossama Mostafa <omostafa1513@gmail.com>
2026-09-10 15:51:31 +0000

    m1a3 updated

--- m1/M1A3.py
+++ m1/M1A3.py
@@ -7,11 +7,11 @@
 
 first_int = int(input("What is the integer? "))
 second_float = float(input("What is the float? "))
-message_str = f"{first_int} + {second_float} is {first_int + second_float} "
+message_str = f"{first_int} + {second_float} is {first_int + second_float}"
 print(message_str)
-message_str = f"{first_int} - {second_float} is {first_int - second_float} "
+message_str = f"{first_int} - {second_float} is {first_int - second_float}"
 print(message_str)
-message_str = f"{first_int} * {second_float} is {first_int * second_float} "
+message_str = f"{first_int} * {second_float} is {first_int * second_float}"
 print(message_str)
-message_str = f"{first_int} / {second_float} is {first_int / second_float} "
+message_str = f"{first_int} / {second_float} is {first_int / second_float}"
 print(message_str)
