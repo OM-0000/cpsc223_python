d3ce7c6234ee274d77e7ffe934e861576af88d1d
Ossama Mostafa <omostafa1513@gmail.com>
2026-09-17 15:57:21 +0000

    updated m2a3)

--- m2/M2A3.py
+++ m2/M2A3.py
@@ -14,8 +14,8 @@ idx_a = int(input("What is the start of your slice? "))
 idx_b = int(input("What is the end of your slice? "))
 data_list = uber_list[idx_a:idx_b]
 slize_sz = len(data_list)
-for idx in range(0, slize_sz):
-    total_int += data_list[idx]
+for item in data_list:
+    total_int += item
 avg = total_int / slize_sz
 
 print(f"Your slice contains {slize_sz} values and has an average value of {avg}")
